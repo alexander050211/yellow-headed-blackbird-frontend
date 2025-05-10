0a4fd1ba468a486a0279a529263d0729ed0f871d
Kim Yeonjun <kyjun050803@gmail.com>
2025-05-10 13:17:03 +0900

    tailwindcss lux mea

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,10 @@ import { useReducer } from 'react';
 export const App = () => {
   const [count, increment] = useReducer((c: number) => c + 1, 0);
 
-  return <button onClick={increment}>{count}</button>;
+  return (
+    <div className="p-10">
+      <div className="text-red-500 font-bold text-lg">Hello World</div>
+      <button onClick={increment}>{count}</button>
+    </div>
+  );
 };
