726bdd49b118e13db798978575a8c90de5c7a63e
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2025-05-10 16:18:51 +0900

    ci fix 2

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Archive = () => {
   return (
     <div className='bg-green-200'>Archaaaive</div>
--- src/pages/current/step2.tsx
+++ src/pages/current/step2.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Step2 = () => {
   return (
     <div>Step2</div>
