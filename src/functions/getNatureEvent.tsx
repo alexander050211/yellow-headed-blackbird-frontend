658ea2689f4866de767534a3cb1cf4ba8e55b731
Kim Yeonjun <kyjun050803@gmail.com>
2025-05-11 06:25:16 +0900

    remove eslint

--- src/functions/getNatureEvent.tsx
+++ src/functions/getNatureEvent.tsx
@@ -3,7 +3,7 @@ export async function getNatureEvents(
   end_date_time: string,
 ) {
   try {
-    const url = `/api/nature-events?start_date_time=${start_date_time}&end_date_time=${end_date_time}`;
+    const url = `/api/nature-events?start_date_time=${start_date_time.slice(0, 19)}&end_date_time=${end_date_time.slice(0, 19)}`;
     const resJson = await fetch(url, {
       method: 'GET',
     }).then((res) => res.json());
